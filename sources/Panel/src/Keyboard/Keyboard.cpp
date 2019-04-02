9a8acebf46beabea10ab1df363664b4bbbd8739a
Sasha7b9 <Sasha7b9@tut.by>
2019-04-02 14:11:37 +0300

    Рефакторинг

--- sources/Panel/src/Keyboard/Keyboard.cpp
+++ sources/Panel/src/Keyboard/Keyboard.cpp
@@ -3,28 +3,33 @@
 #include "stm32f4xx_hal.h"
 #include "Keyboard.h"
 
-Buttons ArrBut[2][4] = {{Button_0, Button_1, Button_2, Button_4},{Button_5, Button_6, Button_7, Button_8}}; 
-Buttons button; 
-int ChoisePin();
-int ChoiseButton(int);
-void Set_All_SL(int);
-void Set_SL(int, int);
-int Read_RL(int);
+Buttons ArrBut[2][4] =
+{
+    {Button_0, Button_1, Button_2, Button_4},
+    {Button_5, Button_6, Button_7, Button_8}
+};
+
+static Buttons button; 
+static void Set_All_SL(int);
+static void Set_SL(int, int);
+static bool LowLevel_RL(int);
 
 Buttons Keyboard::Update()
 {
-    for(int bus=0; bus<4; bus++)
+    for(int bus = 0; bus < 4; bus++)
     {
         Set_All_SL(1);
         Set_SL(bus, 0);
-        for(int pin=0; pin<4; pin++)
+        for(int pin = 0; pin < 4; pin++)
         {
-            int state = Read_RL(pin);
-            if (state == 0)
+            if (LowLevel_RL(pin))
             {
-             button = ArrBut[bus][pin];
+                button = ArrBut[bus][pin];
             }
-            else button = Button_NULL;
+            else
+            {
+                button = Button_NULL;
+            }
         }
     }
     return button;
@@ -61,18 +66,10 @@ void Keyboard::Init()
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-    
-    //HAL_GPIO_WritePin(GPIOC, GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15, GPIO_PIN_SET);
-    //HAL_GPIO_WritePin(GPIOD, GPIO_PIN_8, GPIO_PIN_SET);
-    //HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12|GPIO_PIN_13, GPIO_PIN_SET);
-    //HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8|GPIO_PIN_9|GPIO_PIN_10, GPIO_PIN_SET);
 }
 
 void Set_All_SL(int st)
 {
-     //GPIO_PinState state [2] = {GPIO_PIN_RESET, GPIO_PIN_SET};
-     //HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12|GPIO_PIN_13|GPIO_PIN_14|GPIO_PIN_15, state[st]);
-     //HAL_GPIO_WritePin(GPIOD, GPIO_PIN_8, state[st]);
     for(int i = 0; i<5; i++)
     {
         Set_SL(i, st);
@@ -81,16 +78,18 @@ void Set_All_SL(int st)
    
 void Set_SL(int bus, int st)
 {
-    GPIO_TypeDef *ports[5]= {GPIOB, GPIOB, GPIOB, GPIOB, GPIOD};
-    uint16_t pins[5] = {GPIO_PIN_12, GPIO_PIN_13, GPIO_PIN_14, GPIO_PIN_15,GPIO_PIN_8};
-    GPIO_PinState state [2] = {GPIO_PIN_RESET, GPIO_PIN_SET};
-    HAL_GPIO_WritePin(ports[bus], pins[bus], state[st]);
+    static const GPIO_TypeDef *ports[5]= {GPIOB, GPIOB, GPIOB, GPIOB, GPIOD};
+    static const uint16_t pins[5] = {GPIO_PIN_12, GPIO_PIN_13, GPIO_PIN_14, GPIO_PIN_15,GPIO_PIN_8};
+    static const GPIO_PinState state [2] = {GPIO_PIN_RESET, GPIO_PIN_SET};
+    
+    HAL_GPIO_WritePin((GPIO_TypeDef *)ports[bus], pins[bus], state[st]);
 }
 
-int Read_RL(int pin)
+bool LowLevel_RL(int pin)
 {
-    GPIO_TypeDef *ports[5]= {GPIOA, GPIOA, GPIOA, GPIOD, GPIOD};
-    uint16_t pins[5] = {GPIO_PIN_8, GPIO_PIN_9, GPIO_PIN_10, GPIO_PIN_13,GPIO_PIN_12};
-    return HAL_GPIO_ReadPin(ports[pin], pins[pin]);
+    static const GPIO_TypeDef *ports[5]= {GPIOA, GPIOA, GPIOA, GPIOD, GPIOD};
+    static const uint16_t pins[5] = {GPIO_PIN_8, GPIO_PIN_9, GPIO_PIN_10, GPIO_PIN_13,GPIO_PIN_12};
+    
+    return HAL_GPIO_ReadPin((GPIO_TypeDef *)ports[pin], pins[pin]) == GPIO_PIN_RESET;
 }
    
